4bca367f0fc317e676195993424841d87ac95fae
Buket Tosun <buket.tosun97@gmail.com>
2024-11-15 14:01:33 +0000

    Test

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.65.221.24';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = ':3ERJnM9(*/uku%4d';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
